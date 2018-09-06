e366a8a2ffc4620201024c9a9a3c538ee3e163a4
nanguazhude <nanguazhude@vip.qq.com>
2018-09-06 15:22:32 +0800

    +注释

--- chapter02/computeshaderaddbuffer/Window.cpp
+++ chapter02/computeshaderaddbuffer/Window.cpp
@@ -66,8 +66,8 @@ void Window::initializeGL() {
 #version 450
 layout (local_size_x = 1 , local_size_y = 1 , local_size_z = 1) in;
 
-layout( binding = 0, std430 ) buffer InputData { vec4 inputData[] ; };
-layout( binding = 1, std430 ) buffer OutputData{ vec4 outputData[]; };
+layout( binding = 0, std430 ) readonly buffer InputData { vec4 inputData[] ; };
+layout( binding = 1, std430 ) writeonly buffer OutputData{ vec4 outputData[]; };
 
 void main(void){
     outputData[ gl_LocalInvocationID.x ] = inputData[gl_LocalInvocationID.x] + vec4(1) ; 
@@ -158,7 +158,9 @@ void Window::resizeGL(int w, int h) {
     glViewport(0, 0, w, h);
 }
 
-
+/***
+https://blog.csdn.net/z444_579/article/details/54138504
+***/
 
 
 
