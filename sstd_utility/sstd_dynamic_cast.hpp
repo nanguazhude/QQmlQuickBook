2199b4af4f84dd26385c2deb98c41b57954a9d92
nanguazhude <nanguazhude@vip.qq.com>
2018-09-17 21:11:56 +0800

    +update speed

--- sstd_utility/sstd_dynamic_cast.hpp
+++ sstd_utility/sstd_dynamic_cast.hpp
@@ -5,23 +5,23 @@
 
 namespace sstd {
     namespace $p$Private {
-        template<typename F,typename T,typename = void>
+        template<typename F, typename T, typename = void>
         class IsStaticCast : public std::false_type {};
         template<typename F, typename T>
-        class IsStaticCast<F,T,std::void_t<
-            decltype( static_cast<T>( std::declval<F>() ) )
+        class IsStaticCast<F, T, std::void_t<
+            decltype(static_cast<T>(std::declval<F>()))
         > > : public std::true_type {};
     }/*$p$Private*/
 }/*namespace sstd*/
 
-template<typename T, bool F = false/*force dynamic_cast?*/,typename U>
+template<typename T, bool F = false/*force dynamic_cast?*/, typename U>
 inline T sstd_dynamic_cast(U * arg) {
     if (arg == nullptr) { return nullptr; }
     using R = std::remove_cv_t< std::remove_reference_t<U> >;
     if constexpr (std::is_convertible_v<R*, T>) {/*up cast ?*/
         return static_cast<T>((R*)(arg));
     }
-    else if constexpr ((F==false)&&
+    else if constexpr ((F == false) &&
         (sstd::$p$Private::IsStaticCast<R*, T>::value)) {/*down cast ?*/
         /*in most time this function is work ,but sometime the function run error!*/
         assert(dynamic_cast<T>((R*)(arg)) == static_cast<T>((R*)(arg)));
@@ -37,7 +37,15 @@ inline T sstd_dynamic_cast(U * arg) {
     }
 }
 
+template<typename T, typename U>
+inline T sstd_reinterpret_dynamic_cast(U * arg) {
+    return sstd_dynamic_cast<T, false, U>(arg);
+}
 
+template<typename T, typename U>
+inline T sstd_safe_dynamic_cast(U * arg) {
+    return sstd_dynamic_cast<T, true, U>(arg);
+}
 
 
 
