b44725e7bc3237610b7227145bb4dd5cbbd5f986
nanguazhude <nanguazhude@vip.qq.com>
2018-10-13 12:01:41 +0800

    +test code

--- chapter02/computeshaderthreaddraw/RootWindow.cpp
+++ chapter02/computeshaderthreaddraw/RootWindow.cpp
@@ -3,13 +3,35 @@
 #include "RenderSize.hpp"
 
 RootWindow::RootWindow() {
+    /**/
+    mmm_Mutex = sstd::make_shared< MutexPointer::element_type >();
+    /*创建OpenGL渲染环境*/
+
+    /**/
     this->setTitle(trUtf8(u8R"(Root Window)"));
     this->setMinimumSize({ getRenderWidth(),getRenderHeight() });
     this->setMaximumSize({ getRenderWidth(),getRenderHeight() });
 }
 
+RootWindow::~RootWindow() {
 
+    auto varMutex = std::move( mmm_Mutex );
 
+    /*rending need this data , so wait for rending finished*/
+    for(;;){
+        std::unique_lock varDestoryLock{ *varMutex };
+        varMutex->isDesotry=true;
+        if( varMutex->isRending ){ continue; }
+        else{ break; }
+    }
+
+    /*destory the contex*/
+    if(this->getContex()) {
+        this->getContex()->doneCurrent();
+        this->getContex()->deleteLater();
+    }
+
+}
 
 
 
--- chapter02/computeshaderthreaddraw/RootWindow.hpp
+++ chapter02/computeshaderthreaddraw/RootWindow.hpp
@@ -1,12 +1,29 @@
 ﻿#pragma once
 
+#include <mutex>
 #include <sstd_memory.hpp>
 #include <QtGui/qwindow.h>
+#include <QtGui/qopenglcontext.h>
 
 class RootWindow : public QWindow {
     Q_OBJECT
 public:
     RootWindow();
+    ~RootWindow();
+
+    class ClassState : public std::recursive_mutex {
+    public:
+        bool isDesotry{false};
+        bool isRending{false};
+    };
+
+    using MutexPointer = std::shared_ptr< ClassState >;
+    QOpenGLContext * getContex() const { return mmm_Contex; }
+    MutexPointer getMutex() const { return mmm_Mutex; }
+
+private:
+    QOpenGLContext * mmm_Contex{ nullptr };
+    MutexPointer     mmm_Mutex;
 private:
     SSTD_MEMORY_QOBJECT_DEFINE(RootWindow)
 };
