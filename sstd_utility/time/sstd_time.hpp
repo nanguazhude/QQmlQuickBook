a24b7695c089ba32ce495cff22307ce52bbd4322
nanguazhude <nanguazhude@vip.qq.com>
2018-10-15 12:19:26 +0800

    +修改注释

--- sstd_utility/time/sstd_time.hpp
+++ sstd_utility/time/sstd_time.hpp
@@ -15,7 +15,7 @@ namespace sstd{
         using type = std::uint64_t;
         /*返回一个时间戳，每次时间戳变化间隔应当小于1ms*/
         virtual type getCurrentTime() const = 0;
-        /*时间戳发生变化时运行此函数一次，然后此函数被清除,此函数抛出的异常将被忽略*/
+        /*时间戳发生变化时运行此函数一次，然后此函数被清除,此函数抛出的异常将被忽略,函数执行顺序不被保证*/
         virtual void postFunction(std::function<void(void)>) =0 ;
     };
 
