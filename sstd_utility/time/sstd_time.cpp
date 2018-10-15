8aec646b701b2baef211dead681f3e3ec733ebcb
ngzHappy <zhaixueqiang@hotmail.com>
2018-10-15 13:14:16 +0800

    +单一执行线程

--- sstd_utility/time/sstd_time.cpp
+++ sstd_utility/time/sstd_time.cpp
@@ -20,12 +20,59 @@ namespace sstd::private_thread {
         return varAns;
     }
 
-    class PrivateTimerThread : public TimerThread {
+    class PrivateTimerThread :
+        public std::enable_shared_from_this<PrivateTimerThread>,
+        public TimerThread {
         std::atomic<type> mmm_Value{ (type)(std::time(nullptr)) };
         std::atomic<bool> & mmm_IsQuit{ isMainQuit() };
         std::shared_mutex mmm_Mutex_Functions;
         sstd::list<std::function<void(void)>> mmm_Functions;
+        std::atomic<int> mmm_RunThreadCount{ 0 };
     public:
+
+        class Runner final {
+            std::shared_ptr<PrivateTimerThread> mmm_Super;
+            sstd::list<std::function<void(void)>> mmm_Functions;
+        public:
+
+            Runner(PrivateTimerThread * r) {
+                r->addRunner();
+                mmm_Super = r->shared_from_this();
+            }
+
+            ~Runner() {
+                mmm_Super->removeRunner();
+            }
+
+            inline void run() {
+                auto & argFunctions = mmm_Functions;
+                while (argFunctions.empty() == false) {
+                    try {
+                        auto varFunction = std::move(argFunctions.front());
+                        argFunctions.pop_front();
+                        varFunction();
+                    } catch (...) {
+                        /*忽略所有异常*/
+                    }
+                }
+            }
+
+            template<typename U>
+            inline void setFunctions(U&&arg) {
+                mmm_Functions = std::forward<U>(arg);
+            }
+
+        private:
+            SSTD_MEMORY_DEFINE(Runner)
+        };
+
+        void addRunner() {
+            ++mmm_RunThreadCount;
+        }
+
+        void removeRunner() {
+            --mmm_RunThreadCount;
+        }
 
         void quit() {
             mmm_IsQuit.store(true);
@@ -42,8 +89,14 @@ namespace sstd::private_thread {
 
         void run() {
             while (mmm_IsQuit.load() == false) {
-                std::this_thread::sleep_for(100us);
+                std::this_thread::sleep_for(500us);
                 ++mmm_Value;
+
+                /*确保只额外启动一个执行线程,如果还有执行线程在运行，那么就等下次执行...*/
+                if (mmm_RunThreadCount.load() > 0) {
+                    continue;
+                }
+
                 /*测试执行是否为空...*/
                 {
                     std::shared_lock varReadLock{ mmm_Mutex_Functions };
@@ -51,25 +104,20 @@ namespace sstd::private_thread {
                         continue;
                     }
                 }
+
+                /*构造一个执行器*/
+                auto varRunner = sstd::make_shared< Runner >(this);
+
                 /*将执行函数拷贝出来...*/
-                std::remove_cv_t<std::remove_reference_t<decltype(mmm_Functions)>> varFunctions;
                 {
                     std::unique_lock varWriteLock{ mmm_Mutex_Functions };
-                    varFunctions = std::move(mmm_Functions);
+                    varRunner->setFunctions(std::move(mmm_Functions));
                 }
+
                 /*运行执行函数...*/
-                std::thread([argFunctions = std::move(varFunctions)]() mutable {
-                    while (argFunctions.empty() == false) {
-                        try {
-                            auto varFunction = std::move(argFunctions.front());
-                            argFunctions.pop_front();
-                            varFunction();
-                        } catch (...) {
-                            /*忽略所有异常*/
-                        }
-                    }
-                }).detach();
-            }
+                std::thread([varRunner]() mutable { varRunner->run(); }).detach();
+
+            }/*while*/
         }
 
         PrivateTimerThread() {
