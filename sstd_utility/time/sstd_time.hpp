5360ca019615699bd61192ffeeefecc52ebd7c7e
ngzHappy <zhaixueqiang@hotmail.com>
2018-10-15 13:50:05 +0800

    +修改注释

--- sstd_utility/time/sstd_time.hpp
+++ sstd_utility/time/sstd_time.hpp
@@ -13,9 +13,15 @@ namespace sstd {
     public:
         virtual ~TimerThread() = default;
         using type = std::uint64_t;
-        /*返回一个时间戳，每次时间戳变化间隔应当小于1ms*/
+        /*返回一个时间戳，
+        每次时间戳变化间隔应当小于1ms*/
         virtual type getCurrentTime() const = 0;
-        /*时间戳发生变化时运行此函数一次，然后此函数被清除,此函数抛出的异常将被忽略,函数执行顺序不被保证*/
+        /*时间戳发生变化时运行此函数一次，
+        然后此函数被清除,
+        此函数抛出的异常将被忽略,
+        函数执行顺序不被保证
+        函数执行线程不保证
+        函数插入时间点和函数执行时间点间隔不保证*/
         virtual void postFunction(std::function<void(void)>) = 0;
         /*返回队列中还存有多少函数*/
         virtual std::size_t functionsAboutToRun() const = 0;
