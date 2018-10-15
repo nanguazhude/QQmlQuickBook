aa31e9a8ac7f1a8cde63f8662daebe9fca454127
ngzHappy <zhaixueqiang@hotmail.com>
2018-10-15 23:12:35 +0800

    +add boost

---
+++ sstd_utility/sstd/readme.txt
@@ -0,0 +1,25 @@
+﻿1. 命令行进入下载的boost包的根目录
+2. bootstrap.bat，得到bjam.exe
+3. bjam tools/bcp，在dist\bin目录下会得到bcp.exe
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ sstd_utility/sstd_boost.hpp
@@ -0,0 +1,14 @@
+﻿#pragma once
+
+#include "sstd/boost/circular_buffer.hpp"
+
+/*Boost Macro Reference*/
+//https://www.boost.org/doc/libs/1_68_0/libs/config/doc/html/boost_config/boost_macro_reference.html
+
+
+
+
+
+
+
+
