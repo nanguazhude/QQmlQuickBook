f783bc61aa325f25384697b93a9d736ced5cbcd6
ngzHappy <zhaixueqiang@hotmail.com>
2018-10-15 20:37:48 +0800

    +注释

--- chapter02/computeshaderaddbuffer/Window.cpp
+++ chapter02/computeshaderaddbuffer/Window.cpp
@@ -65,11 +65,11 @@ void Window::initializeGL() {
 #version 450
 layout (local_size_x = 1 , local_size_y = 1 , local_size_z = 1) in;
 
-layout( binding = 0, std430 ) readonly buffer InputData { vec4 inputData[] ; };
+layout( binding = 0, std430 ) readonly  buffer InputData { vec4 inputData[] ; };
 layout( binding = 1, std430 ) writeonly buffer OutputData{ vec4 outputData[]; };
 
 void main(void){
-    outputData[ gl_LocalInvocationID.x ] = inputData[gl_LocalInvocationID.x] + vec4(1) ; 
+    outputData[ gl_LocalInvocationID.x ] = inputData[gl_LocalInvocationID.x] + vec4(1) ;
 }
 
 )___" };
@@ -107,7 +107,7 @@ void Window::paintGL() {
     /*为了照顾数学不好的孩子，这里只让显卡做2以内的加法*/
     GLfloat varInput[4];
     GLfloat varOutput[4];
-    
+
     varInput[0] = std::rand() & 1;
     varInput[1] = std::rand() & 1;
     varInput[2] = std::rand() & 1;
