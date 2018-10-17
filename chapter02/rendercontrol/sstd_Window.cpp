329c93dee4f16f1aeced6f189ae3ed1db6af6eeb
nanguazhude <nanguazhude@vip.qq.com>
2018-10-17 09:59:46 +0800

    +test.code.001

--- chapter02/rendercontrol/sstd_Window.cpp
+++ chapter02/rendercontrol/sstd_Window.cpp
@@ -1,10 +1,40 @@
 ﻿#include "sstd_Window.hpp"
+#include <ConstructQSurface.hpp>
 
 namespace sstd {
 
     Window::Window(){
+
+        mmm_Mutex = sstd::make_shared<ClassState>();
+
         this->setMinimumHeight(128);
         this->setMinimumWidth(128);
+
+        /*创建OpenGL渲染环境*/
+        this->setSurfaceType(QSurface::OpenGLSurface);
+        this->setFormat(sstd::getDefaultOpenGLFormat());
+
+    }
+
+    Window::~Window() {
+
+        auto varMutex = mmm_Mutex;
+        varMutex->setDestory();
+
+        /*rending need this data , so wait for rending finished*/
+        for (;;) {
+            if (varMutex->renderCount() > 0) {
+                std::this_thread::sleep_for(10ns);
+            } else {
+                break;
+            }
+        }
+
+        /*destory the contex*/
+        if (this->getContex()) {
+            this->getContex()->deleteLater();
+        }
+
     }
 
 } // namespace sstd
--- chapter02/rendercontrol/sstd_Window.hpp
+++ chapter02/rendercontrol/sstd_Window.hpp
@@ -1,5 +1,6 @@
 ﻿#pragma once
 
+#include <shared_mutex>
 #include <sstd_memory.hpp>
 #include <QtGui/qwindow.h>
 
@@ -9,6 +10,49 @@ namespace sstd {
         Q_OBJECT
     public:
         Window();
+        ~Window();
+
+        class ClassState final : public std::shared_mutex {
+            SSTD_DELETE_COPY_ASSIGN(ClassState);
+        private:
+            friend Window;
+            using This = ClassState;
+        protected:
+            bool mmm_IsDesotry{ false };
+            int mmm_RendingCount{ false };
+            void setDestory() {
+                std::unique_lock varWriteLock{ *const_cast<This*>(this) };
+                mmm_IsDesotry = true;
+            }
+        public:
+            ClassState() = default;
+            bool isDestory() const volatile {
+                std::shared_lock varReadLock{ *const_cast<This*>(this) };
+                return mmm_IsDesotry;
+            }
+            int renderCount() const volatile {
+                std::shared_lock varReadLock{ *const_cast<This*>(this) };
+                return mmm_RendingCount;
+            }
+            void addRenderCount() {
+                std::unique_lock varWriteLock{ *const_cast<This*>(this) };
+                ++mmm_RendingCount;
+            }
+            void subRenderCount() {
+                std::unique_lock varWriteLock{ *const_cast<This*>(this) };
+                --mmm_RendingCount;
+            }
+        private:
+            SSTD_MEMORY_DEFINE(ClassState)
+        };
+
+        QOpenGLContext * getContex() const {
+            return mmm_Contex;
+        }
+
+    private:
+        QOpenGLContext * mmm_Contex{ nullptr };
+        std::shared_ptr<ClassState> mmm_Mutex;
     private:
         SSTD_MEMORY_QOBJECT_DEFINE(Window)
     };
