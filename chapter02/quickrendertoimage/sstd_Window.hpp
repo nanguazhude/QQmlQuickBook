aa11ff348b0ef777c772089f80ffb91683237979
ngzHappy <zhaixueqiang@hotmail.com>
2018-10-21 08:58:55 +0800

    +test code

--- chapter02/quickrendertoimage/sstd_Window.hpp
+++ chapter02/quickrendertoimage/sstd_Window.hpp
@@ -9,33 +9,11 @@ namespace sstd {
 
     class Window : public QQuickView {
         Q_OBJECT
-    private:
-        using Super = QQuickView;
     public:
         Window();
         ~Window();
-
-    protected:
-        void exposeEvent(QExposeEvent *e) override;
-        void resizeEvent(QResizeEvent *e) override;
-        void mousePressEvent(QMouseEvent *e) override;
-        void mouseReleaseEvent(QMouseEvent *e) override;
-        bool event(QEvent *e) override;
-
-    private :
-        Q_SLOT void requestUpdate();
-        Q_SLOT void justRender();
-        Q_SLOT void polishSyncAndRender();
-        Q_SLOT void polishSyncAndRenderResize();
-        bool isResizing() const;
     private:
-        template<bool needPolish,bool needSync,bool isResize>
-        void ppp_PolishSyncAndRender();
-    private:
-        void startQuick(const QUrl &filename);
-        void updateSizes();
-        bool mmm_QuickInitialized;
-        std::shared_ptr<sstd::RenderPack> mmm_RenderPack;
+        SSTD_MEMORY_QOBJECT_DEFINE(Window)
     };
 
 }/*namespace sstd*/
