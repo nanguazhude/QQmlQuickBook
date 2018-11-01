186d0f28c9d7730691d4bcb2237b2122d62280fe
ngzHappy <zhaixueqiang@hotmail.com>
2018-11-01 10:45:56 +0800

    +

--- chapter04/quickchartdeep/RootWindow.cpp
+++ chapter04/quickchartdeep/RootWindow.cpp
@@ -2,7 +2,7 @@
 #include "RootWindow.hpp"
 
 RootWindow::RootWindow(){
-    setTitle( trUtf8( u8R"(Hellow World!)" ) );
+    setTitle( trUtf8( u8R"(物体与坐标关联)" ) );
     this->setMinimumSize({256,256});
     this->setClearColor(QColor(0,0,0,1));
 }
