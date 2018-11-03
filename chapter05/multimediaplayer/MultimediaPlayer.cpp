6f37a5ea982d41cc38bcaf566a2b1323f76a047b
nanguazhude <nanguazhude@vip.qq.com>
2018-11-03 12:24:15 +0800

    测试代码

--- chapter05/multimediaplayer/MultimediaPlayer.cpp
+++ chapter05/multimediaplayer/MultimediaPlayer.cpp
@@ -1,4 +1,58 @@
-﻿
+﻿#include "MultimediaPlayer.hpp"
+#include <QtGui/qimage.h>
+/**********************************/
+/*include ffmpeg*/
+
+/**********************************/
+namespace this_cpp_file {
+    
+    class FrameImage {
+    public:
+        int width {0};
+        int height{0};
+        double pts{0};
+
+    };
+
+
+
+}/*namespace this_cpp_file*/
+
+namespace sstd {
+
+    Player::Player() {
+
+        mmm_IsStart = create_object_in_this_class<bool>(false);
+        mmm_ISLocalFile = create_object_in_this_class<bool>(true);
+        mmm_LocalFileName = create_object_in_this_class<QString>();
+
+    }
+    
+    void Player::setLocalFile(const QString & arg) {
+        assert((*mmm_IsStart)==false);
+        (*mmm_ISLocalFile) = true;
+        (*mmm_LocalFileName) = arg;
+    }
+
+    bool Player::start() {
+        assert((*mmm_IsStart) == false);
+        (*mmm_IsStart) = true;
+        if (*mmm_ISLocalFile) {
+           return ppp_start_local();
+        } else {
+           return ppp_start_net();
+        }
+    }
+    
+    bool Player::ppp_start_local() {
+
+    }
+
+    bool Player::ppp_start_net() {
+
+    }
+
+}/*namespace sstd*/
 
 
 
--- chapter05/multimediaplayer/MultimediaPlayer.hpp
+++ chapter05/multimediaplayer/MultimediaPlayer.hpp
@@ -1,19 +1,32 @@
 ﻿#pragma once
 #include <QObject>
+#include <sstd_memory.hpp>
 
-namespace sstd{
-
-
-
-class Player : public QObject {
-    Q_OBJECT
-public:
-
-    void setLocalFile(const QString &);
+namespace sstd {
 
 
 
-};
+    class Player :
+        public QObject,
+        public virtual sstd::memory_lock::VirtualClassBasic {
+        Q_OBJECT
+    public:
+
+        void setLocalFile(const QString &);
+        bool start();
+
+    public:
+        Player();
+    private:
+        bool * mmm_IsStart;
+        bool * mmm_ISLocalFile;
+        QString * mmm_LocalFileName;
+    private:
+        bool ppp_start_local();
+        bool ppp_start_net();
+    private:
+        SSTD_MEMORY_QOBJECT_DEFINE(Player)
+    };
 
 
 
