a213e49bffd6c5785ce3298a53a6c96852f4ba4d
nanguazhude <nanguazhude@vip.qq.com>
2018-11-08 10:09:12 +0800

    +test code

--- chapter01/function/boost_context.cpp
+++ chapter01/function/boost_context.cpp
@@ -3,8 +3,45 @@
 
 
 extern void test_boost_context();
+#include <iostream>
 
-void test_boost_context(){
+void test_boost_context() {
+
+    {
+        using fiber = boost::context::fiber;
+        fiber f([](fiber && f)->fiber {
+            std::cout << "Hellow World"sv << std::endl;
+            return std::move(f);
+        });
+
+        std::move(f).resume();
+    }
+
+    {
+        using fiber = boost::context::fiber;
+        fiber f([](fiber && f)->fiber {
+            std::cout << "1"sv << std::endl;
+            f = std::move(f).resume();
+            std::cout << "3"sv << std::endl;
+            return std::move(f);
+        });
+
+        f = std::move(f).resume();
+        std::cout << "2"sv << std::endl;
+        f = std::move(f).resume();
+
+    }
+
+    {
+        using fiber = boost::context::fiber;
+        fiber f(std::allocator_arg, boost::context::fixedsize_stack{ 8*1024 }, [](fiber && f)->fiber {
+            std::cout << "Hellow World"sv << std::endl;
+            return std::move(f);
+        });
+
+        f = std::move(f).resume();
+    }
+
 
 }
 
