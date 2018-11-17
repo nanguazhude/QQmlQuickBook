ba40f2c71aac59f17a8406397c36e9fa71dce083
ngzHappy <zhaixueqiang@hotmail.com>
2018-11-17 17:23:55 +0800

    ++++

---
+++ sstd_virtual_basic.cpp
@@ -0,0 +1,127 @@
+﻿#include "sstd_virtual_basic.hpp"
+#include <cassert>
+
+const sstd_virtual_basic::sstd_type_index & sstd_virtual_basic::sstd_get_type_index() const {
+    using sstd_this_type_ = std::remove_cv_t<
+        std::remove_reference_t< decltype(*this) > >;
+    return ::sstd_get_type_index<sstd_this_type_>();
+}
+
+sstd_virtual_basic::sstd_virtual_basic() noexcept {
+    mmm_objects = new std::map< sstd_type_index, void * >;
+    sstd_add_object_cast(this->sstd_get_type_index(), this);
+}
+
+sstd_virtual_basic::~sstd_virtual_basic() {
+    destory_objects_in_this();
+    delete mmm_named_objects;
+    delete mmm_objects_in_this;
+    delete mmm_objects;
+    delete mmm_mutex;
+}
+
+void sstd_virtual_basic::sstd_add_object_cast(const sstd_type_index & k, void * v) {
+    mmm_objects->emplace(k, v);
+}
+
+void * sstd_virtual_basic::sstd_find_object(const sstd_type_index & k) const {
+    auto varPos = (std::as_const(*mmm_objects).find(k));
+    if (varPos == (std::as_const(*mmm_objects).end())) {
+        return nullptr;
+    }
+    return varPos->second;
+}
+
+sstd_virtual_basic::maped_named_objects_t * sstd_virtual_basic::get_named_objects() noexcept {
+    if (mmm_named_objects == nullptr) {
+        mmm_named_objects = new maped_named_objects_t_t;
+    }
+    return mmm_named_objects->get_type_data();
+}
+
+sstd_virtual_basic::mmm_objects_in_this_t * sstd_virtual_basic::get_objects_list() noexcept {
+    if (mmm_objects_in_this == nullptr) {
+        mmm_objects_in_this = new mmm_objects_in_this_t_t;
+    }
+    return mmm_objects_in_this->get_type_data();
+}
+
+void sstd_virtual_basic::destory_objects_in_this() noexcept {
+    if (mmm_objects_in_this) {
+        auto p = mmm_objects_in_this->get_type_data();
+        while (p->empty() == false) {
+            p->pop_front();
+        }
+    }
+}
+
+void sstd_virtual_basic::insert_object(std::unique_ptr< private_object_t  > && arg) noexcept {
+    get_objects_list()->push_front(std::move(arg));
+}
+
+void sstd_virtual_basic::insert_named_object(const std::string_view &name, std::unique_ptr< private_object_t  > && arg) noexcept {
+    get_named_objects()->emplace(name, arg.get());
+    get_objects_list()->push_front(std::move(arg));
+}
+
+void * sstd_virtual_basic::sstd_find_named_object(const std::string_view &name) const {
+    auto varMap = const_cast<sstd_virtual_basic *>(this)->get_named_objects();
+    auto varPos = varMap->find(name);
+    if (varPos == varMap->end()) {
+        return nullptr;
+    }
+    return varPos->second->get_data();
+}
+
+void _01_00_private_sstd_virtual_basic::named_object::set_name(const std::string_view & name) {
+    *(mmm_name->get_type_data()) = name;
+}
+
+std::string_view _01_00_private_sstd_virtual_basic::named_object::get_name() const {
+    return *(mmm_name->get_type_data());
+}
+
+_01_00_private_sstd_virtual_basic::named_object::named_object() noexcept {
+    mmm_name = new string_type;
+}
+
+_01_00_private_sstd_virtual_basic::named_object::~named_object() noexcept {
+    delete mmm_name;
+}
+
+_01_00_private_sstd_virtual_basic::object::~object() noexcept {
+}
+
+_01_00_private_sstd_virtual_basic::object::object() noexcept {
+}
+
+std::shared_ptr< std::recursive_mutex > sstd_virtual_basic::sstd_get_class_mutex() const noexcept {
+    auto varAns = mmm_mutex.load();
+    if (varAns) {
+        return *(varAns->get_type_data());
+    }
+
+    {
+        auto varData = new mutex_t_t;
+        (*(varAns->get_type_data())) = std::make_shared< std::recursive_mutex >();
+        mutex_t_t * varTmp{ nullptr };
+        const bool varSetValue = mmm_mutex.compare_exchange_strong(varTmp, varAns);
+        if (varSetValue) {
+            varAns = varData;
+        } else {
+            delete varData;
+            varAns = varTmp;
+            assert(varAns == nullptr);
+        }
+    }
+
+    return *(varAns->get_type_data());
+}
+
+
+
+
+
+
+
+
--- sstd_virtual_basic.hpp
+++ sstd_virtual_basic.hpp
@@ -1,18 +1,74 @@
 ﻿#pragma once
 
 #include <map>
+#include <mutex>
+#include <atomic>
+#include <string>
 #include <utility>
 #include <cstddef>
 #include <cstdint>
 #include <typeinfo>
 #include <typeindex>
 #include <type_traits>
+#include <string_view>
+#include <forward_list>
+
+namespace _01_00_private_sstd_virtual_basic {
+    template<typename T, typename U>
+    class alignas((alignof(T) > alignof(void *)) ? alignof(T) : alignof(void *)) object_wrap final : public U{
+        T mmm_data;
+        static_assert(false == std::is_reference_v<T>);
+        static_assert(false == std::is_array_v<T>);
+    public:
+        inline void * get_data() const override {
+            return get_type_data();
+        }
+        inline T * get_type_data() const {
+            return const_cast<T *>(&mmm_data);
+        }
+        template<typename A0, typename ... A,
+            typename = std::enable_if_t<
+            true == std::is_constructible_v<T, A0&&, A&&...>> >
+            inline object_wrap(A0&&a0, A && ... args) : mmm_data(std::forward<A0>(a0),
+                std::forward<A>(args)...) {
+        }
+        template<typename A0, typename ... A,
+            typename = decltype(nullptr),
+            typename = std::enable_if_t<
+            false == std::is_constructible_v<T, A0&&, A&&...>> >
+            inline object_wrap(A0&&a0, A && ... args) : mmm_data{ std::forward<A0>(a0),
+                std::forward<A>(args)... } {
+        }
+        inline object_wrap() : mmm_data{} {
+        }
+    };
+    class object {
+    public:
+        virtual void * get_data() const = 0;
+        virtual ~object() noexcept;
+        object(const object &) = delete;
+        object(object &&) = delete;
+        object&operator=(const object &) = delete;
+        object&operator=(object&&) = delete;
+        object() noexcept;
+    };
+    class named_object : public object {
+        using string_type = object_wrap<std::string, object>;
+        string_type * mmm_name{ nullptr };
+    public:
+        named_object() noexcept;
+        virtual ~named_object() noexcept;
+        void set_name(const std::string_view &);
+        std::string_view get_name() const;
+    };
+}/*namespace*/
 
 template<typename T>
 class sstd_register_virtual_basic;
 class sstd_virtual_basic {
 protected:
     using type_index_t = std::type_index;
+    using private_object_t = _01_00_private_sstd_virtual_basic::object;
 public:
     using sstd_type_index = std::pair<std::size_t, type_index_t>;
     template<typename T>
@@ -26,65 +82,85 @@ public:
         }();
         return varAns;
     }
+    std::shared_ptr< std::recursive_mutex > sstd_get_class_mutex() const noexcept /*此函数线程安全*/;
 private:
-    std::map< sstd_type_index, void * > mmm_objects;
+    using mutex_t_t = _01_00_private_sstd_virtual_basic::object_wrap<std::shared_ptr< std::recursive_mutex >, private_object_t>;
+    using maped_named_objects_t = std::map< std::string_view, private_object_t * >;
+    using mmm_objects_in_this_t = std::forward_list< std::unique_ptr< private_object_t > >;
+    using maped_named_objects_t_t = _01_00_private_sstd_virtual_basic::object_wrap<maped_named_objects_t, private_object_t>;
+    using mmm_objects_in_this_t_t = _01_00_private_sstd_virtual_basic::object_wrap<mmm_objects_in_this_t, private_object_t>;
+    std::map< sstd_type_index, void * > * mmm_objects;
+    maped_named_objects_t_t * mmm_named_objects{ nullptr };
+    mmm_objects_in_this_t_t * mmm_objects_in_this{ nullptr };
+    mutable std::atomic< mutex_t_t * > mmm_mutex{ nullptr };
 public:
     virtual ~sstd_virtual_basic();
     virtual const sstd_type_index & sstd_get_type_index() const;
     inline sstd_virtual_basic * sstd_get_virtual_basic() const {
         return const_cast<sstd_virtual_basic *>(this);
     }
-    inline sstd_virtual_basic() {
-        sstd_add_object_cast(this->sstd_get_type_index(), this);
-    }
+    sstd_virtual_basic() noexcept;
     void * sstd_find_object(const sstd_type_index & k) const;
+    template<typename T, typename ... U>
+    inline T * sstd_create_object_in_this_class(U && ...);
+    template<typename T, typename ... U>
+    inline T * sstd_create_named_object_in_this_class(std::string_view, U && ...);
+    void * sstd_find_named_object(const std::string_view &) const;
 protected:
     void sstd_add_object_cast(const sstd_type_index & k, void * v);
 public:
-    sstd_virtual_basic(const sstd_virtual_basic &) = default;
-    sstd_virtual_basic(sstd_virtual_basic&&) = default;
-    sstd_virtual_basic&operator=(const sstd_virtual_basic &) = default;
-    sstd_virtual_basic&operator=(sstd_virtual_basic&&) = default;
+    sstd_virtual_basic(const sstd_virtual_basic &) = delete;
+    sstd_virtual_basic(sstd_virtual_basic&&) = delete;
+    sstd_virtual_basic&operator=(const sstd_virtual_basic &) = delete;
+    sstd_virtual_basic&operator=(sstd_virtual_basic&&) = delete;
 private:
     template<typename T>
     friend class sstd_register_virtual_basic;
+    maped_named_objects_t * get_named_objects() noexcept;
+    mmm_objects_in_this_t * get_objects_list() noexcept;
+    void destory_objects_in_this() noexcept;
+    void insert_object(std::unique_ptr< private_object_t > &&) noexcept;
+    void insert_named_object(const std::string_view &, std::unique_ptr< private_object_t > &&) noexcept;
 };
+
+template<typename T, typename ... U>
+inline T * sstd_virtual_basic::sstd_create_object_in_this_class(U && ...args) {
+    using sstd_this_type_1 = std::remove_cv_t< T >;
+    using sstd_this_object_1 = _01_00_private_sstd_virtual_basic::object;
+    using sstd_this_wrap_1 = _01_00_private_sstd_virtual_basic::object_wrap<sstd_this_type_1, sstd_this_object_1>;
+    std::unique_ptr varAnsUnique = std::make_unique<sstd_this_wrap_1>(std::forward<U>(args)...);
+    auto varAns = reinterpret_cast<T *>(varAnsUnique->get_data());
+    this->insert_object(std::move(varAnsUnique));
+    return varAns;
+}
+
+template<typename T, typename ... U>
+inline T * sstd_virtual_basic::sstd_create_named_object_in_this_class(std::string_view name, U && ... args) {
+    using sstd_this_type_1 = std::remove_cv_t< T >;
+    using sstd_this_object_1 = _01_00_private_sstd_virtual_basic::named_object;
+    using sstd_this_wrap_1 = _01_00_private_sstd_virtual_basic::object_wrap<sstd_this_type_1, sstd_this_object_1>;
+    std::unique_ptr varAnsUnique = std::make_unique<sstd_this_wrap_1>(std::forward<U>(args)...);
+    auto varAns = reinterpret_cast<T *>(varAnsUnique->get_data());
+    varAnsUnique->set_name(name);
+    name = varAnsUnique->get_name();
+    this->insert_named_object(name, std::move(varAnsUnique));
+    return varAns;
+}
 
 /*typeid*/
 template<typename T>
-inline static const sstd_virtual_basic::sstd_type_index & get_sstd_type_index() noexcept {
+inline static const sstd_virtual_basic::sstd_type_index & sstd_get_type_index() noexcept {
     return sstd_virtual_basic::get_sstd_type_index<T>();
 }
 
 /*dynamic_cast*/
-template<typename T,typename U = sstd_virtual_basic*>
+template<typename T, typename U = sstd_virtual_basic * >
 inline static T * sstd_find_object(const U & arg) {
     if (arg) {
         return reinterpret_cast<T *>(
-            arg->sstd_find_object(::get_sstd_type_index<T>()));
+            arg->sstd_find_object(::sstd_get_type_index<T>()));
     }
     return nullptr;
-}
-
-const sstd_virtual_basic::sstd_type_index & sstd_virtual_basic::sstd_get_type_index() const {
-    using sstd_this_type_ = std::remove_cv_t<
-        std::remove_reference_t< decltype(*this) > >;
-    return ::get_sstd_type_index<sstd_this_type_>();
-}
-
-sstd_virtual_basic::~sstd_virtual_basic() {
-}
-
-void sstd_virtual_basic::sstd_add_object_cast(const sstd_type_index & k, void * v) {
-    mmm_objects.emplace(k, v);
-}
-
-void * sstd_virtual_basic::sstd_find_object(const sstd_type_index & k) const {
-    auto varPos = (std::as_const(mmm_objects).find(k));
-    if (varPos == (std::as_const(mmm_objects).end())) {
-        return nullptr;
-    }
-    return varPos->second;
 }
 
 template<typename T>
@@ -101,14 +177,31 @@ protected:
     }
     inline sstd_virtual_basic * sstd_get_virtual_basic() const {
         using sstd_this_type_ = std::remove_cv_t< std::remove_reference_t< T > >;
-        return static_cast<const sstd_virtual_basic *>( 
+        return static_cast<const sstd_virtual_basic *>(
             static_cast<const sstd_this_type_ *>(this))->sstd_get_virtual_basic();
     }
     inline void * sstd_find_object(const sstd_type_index & k) const {
         return this->sstd_get_virtual_basic()->sstd_find_object(k);
     }
     inline void sstd_add_object_cast(const sstd_type_index & k, void * v) {
-        return this->sstd_get_virtual_basic()->sstd_add_object_cast(k,v);
+        return this->sstd_get_virtual_basic()->sstd_add_object_cast(k, v);
+    }
+    template<typename T111, typename ... U>
+    inline T111 * sstd_create_object_in_this_class(U && ...args) {
+        return this->sstd_get_virtual_basic()
+            ->template sstd_create_object_in_this_class<T111>(std::forward<U>(args)...);
+    }
+    template<typename T111, typename ... U>
+    inline T111 * sstd_create_named_object_in_this_class(const std::string_view & name, U && ...args) {
+        return this->sstd_get_virtual_basic()
+            ->template sstd_create_named_object_in_this_class<T111>(name, std::forward<U>(args)...);
+    }
+    inline void * sstd_find_named_object(const std::string_view & name) const {
+        return this->sstd_get_virtual_basic()->sstd_find_named_object(name);
+    }
+    std::shared_ptr< std::recursive_mutex > sstd_get_class_mutex() const noexcept/*线程安全*/ {
+        return this->sstd_get_virtual_basic()
+            ->sstd_get_class_mutex();
     }
 };
 
@@ -140,6 +233,32 @@ inline void sstd_add_object_cast(const sstd_type_index & k, void * v) { \
     using sstd_this_type_ = std::remove_cv_t< \
         std::remove_reference_t< decltype(*this) > >; \
     return sstd_register_virtual_basic<sstd_this_type_>::sstd_add_object_cast(k, v); \
+} \
+public: \
+inline void * sstd_find_named_object(const std::string_view & name) const { \
+using sstd_this_type_ = std::remove_cv_t< \
+    std::remove_reference_t< decltype(*this) > >; \
+return sstd_register_virtual_basic<sstd_this_type_>::sstd_find_named_object(name); \
+} \
+template<typename T111, typename ... U> \
+inline T111 * sstd_create_named_object_in_this_class(const std::string_view & name, U && ...args) { \
+    using sstd_this_type_ = std::remove_cv_t< \
+        std::remove_reference_t< decltype(*this) > >; \
+    return sstd_register_virtual_basic<sstd_this_type_>:: \
+    template sstd_create_named_object_in_this_class<T111>(name, std::forward<U>(args)...); \
+} \
+template<typename T111, typename ... U> \
+inline T111 * sstd_create_object_in_this_class(U && ...args) { \
+    using sstd_this_type_ = std::remove_cv_t< \
+        std::remove_reference_t< decltype(*this) > >; \
+    return sstd_register_virtual_basic<sstd_this_type_>:: \
+    template sstd_create_object_in_this_class<T111>(std::forward<U>(args)...); \
+} \
+std::shared_ptr< std::recursive_mutex > sstd_get_class_mutex() const noexcept{ \
+    using sstd_this_type_ = std::remove_cv_t< \
+        std::remove_reference_t< decltype(*this) > >; \
+    return sstd_register_virtual_basic<sstd_this_type_>:: \
+        sstd_get_class_mutex(); \
 }
 /***************************************************************/
 #endif
